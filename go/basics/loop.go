a49d518ac93d0bae1ee40327766b85390374b3ad
vanvuvuong <vanvuvuong3@gmail.com>
2024-08-06 13:53:05 +0700

    add loop

--- go/basics/loop.go
+++ go/basics/loop.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func loop() {
+	for j := 0; j < 3; j++ {
+		fmt.Println(j)
+	}
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	var data []map[string]string
+	for key, value := range data {
+		fmt.Println(key)
+		fmt.Println(value)
+	}
+}
