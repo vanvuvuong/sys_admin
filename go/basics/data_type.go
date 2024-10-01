90dc4d17a60db04e1ec2b82cb9ae395533014525
vanvuvuong <vanvuvuong3@gmail.com>
2024-10-01 17:46:26 +0700

    update go datatype example

--- go/basics/data_type.go
+++ go/basics/data_type.go
@@ -1,18 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func define() {
+	// char variable example
 	exampleChar := 'a'
 	fmt.Print(exampleChar)
 
+	// string variable example
 	exampleString := "string"
 	fmt.Print(exampleString)
 
+	// number variable example
 	exampleFloat := 1.11111
 	fmt.Print(exampleFloat)
 	exampleInt := 1
 	fmt.Print(exampleInt)
+
+	// multiple variables declaration
+	exampleInt1, exampleInt2 := 1, 2
+	fmt.Print(exampleInt1, exampleInt2)
 
 	// array: fixed type & len
 	var exampleArray1 [5]int
@@ -25,9 +34,17 @@ func define() {
 		{1: 3},
 	}
 	fmt.Print(exampleArray3)
+
 	// slice: fixed type
 	var exampleSlice []string
 	fmt.Print(exampleSlice)
+
+	// map: dict in Python
+	exampleMap1 := make(map[string]string)
+	exampleMap1["key"] = "value"
+	fmt.Print(exampleMap1)
+	exampleMap2 := map[string]int{"foo": 1, "bar": 2}
+	fmt.Print(exampleMap2)
 
 	// struct ~ equal to dictionary in other language
 	type example_struct_type struct {
