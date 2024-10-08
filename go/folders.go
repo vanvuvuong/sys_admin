afa960c30812ac55c671f27f4a48593f202a00b1
vanvuvuong <vanvuvuong3@gmail.com>
2024-10-08 17:13:53 +0700

    add folder util, update http

--- go/folders.go
+++ go/folders.go
@@ -1,1 +1,9 @@
 package main
+
+import "os"
+
+func CreateFolder(folderPath string) {
+	if err := os.MkdirAll(folderPath, os.ModePerm); err != nil {
+		l(err)
+	}
+}
--- go/http.go
+++ go/http.go
@@ -1,29 +1,53 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"io"
 	"net/http"
 )
 
-func GetRequest() {
-	url := "https://google.com"
-	client := http.Client{}
+var client = http.Client{}
 
+func GetRequest(url, token string) string {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
+		l(err)
 		panic(err)
 	}
-	req.Header.Add("Accept", "application/json")
+	req.Header.Add("user-agent:", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0")
+	req.Header.Add("authorization:", sf("Bearer %s", token))
 	resp, err := client.Do(req)
 	if err != nil {
+		l(err)
 		panic(err)
 	}
 	defer resp.Body.Close()
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		p(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		l(err)
 		panic(err)
 	}
+	return string(body)
+}
+
+func PostRequest(url, token string, payload []byte) string {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		l(err)
+		panic(err)
+	}
+	req.Header.Add("user-agent:", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0")
+	req.Header.Add("authorization:", sf("Bearer %s", token))
+	resp, err := client.Do(req)
+	if err != nil {
+		l(err)
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		l(err)
+		panic(err)
+	}
+	return string(body)
 }
