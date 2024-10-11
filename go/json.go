36baf5ad18e1078924e1daba4d8cbef332463925
vanvuvuong <vanvuvuong3@gmail.com>
2024-10-11 16:05:40 +0700

    update json decode

--- go/json.go
+++ go/json.go
@@ -22,5 +22,25 @@ func EncodeJson() {
 
 func DecodeJson(data []byte) *fjson.Value {
 	jsonData := fjson.MustParse(string(data))
+	var newValue fjson.Arena
+
+	// convert to to object
+	objectJsonData, err := jsonData.Object()
+	Check("Error convert JSON to Object", err)
+
+	// loop over object
+	objectJsonData.Visit(func(key []byte, value *fjson.Value) {
+		pf("%v: %v", key, value)
+	})
+	objectJsonData.Set("example", newValue.NewString("value"))
+
+	// loop over array
+	arrayJsonData, err := jsonData.Array()
+	Check("Error convert JSON to array", err)
+	newArray := newValue.NewArray()
+	for index, object := range arrayJsonData {
+		newArray.SetArrayItem(index, object)
+	}
+
 	return jsonData
 }
