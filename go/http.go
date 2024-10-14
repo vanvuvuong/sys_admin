04c4572e9cb2b3d02dceec423c77c9d0dbbefce5
vanvuvuong <vanvuvuong3@gmail.com>
2024-10-14 10:56:45 +0700

    update http function

--- go/http.go
+++ go/http.go
@@ -8,11 +8,12 @@ import (
 
 var client = http.Client{}
 
-func GetRequest(url, token string) string {
+func GetRequest(url string, headers map[string]string) string {
 	req, err := http.NewRequest("GET", url, nil)
 	Check("Error create request", err)
-	req.Header.Add("user-agent:", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0")
-	req.Header.Add("authorization:", sf("Bearer %s", token))
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
 	resp, err := client.Do(req)
 	Check("Error send request", err)
 	defer resp.Body.Close()
@@ -21,15 +22,24 @@ func GetRequest(url, token string) string {
 	return string(body)
 }
 
-func PostRequest(url, token string, payload []byte) string {
+func PostRequest(url string, headers map[string]string, payload []byte) string {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
 	Check("Error create request", err)
-	req.Header.Add("user-agent:", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0")
-	req.Header.Add("authorization:", sf("Bearer %s", token))
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
 	resp, err := client.Do(req)
 	Check("Error send request", err)
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	Check("Error parse request body", err)
 	return string(body)
+}
+
+func Example(token string) {
+	customHeader := map[string]string{
+		"user-agent":    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0",
+		"authorization": sf("Bearer %s", token),
+	}
+	GetRequest("https://example.com", customHeader)
 }
--- go/json.go
+++ go/json.go
@@ -1,24 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-
 	fjson "github.com/valyala/fastjson"
 )
-
-type Data struct {
-	Id       int64  `json."id"`
-	Category string `json."category"`
-	Name     string `json."name"`
-}
-
-func EncodeJson() {
-	p(">>Encoding struct to Json")
-	data := &Data{Id: 1, Category: "ProgrammingLanguage", Name: "Golang"}
-	encodingData, err := json.Marshal(data)
-	Check("Error encoding JSON", err)
-	p(string(encodingData))
-}
 
 func DecodeJson(data []byte) *fjson.Value {
 	jsonData := fjson.MustParse(string(data))
