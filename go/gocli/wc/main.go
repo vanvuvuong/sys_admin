a117d7602264be5cf10e9f95faa965bc53e44c99
vanvuvuong <vanvuvuong3@gmail.com>
2024-10-21 16:11:36 +0700

    add go word count bytes

--- go/gocli/wc/main.go
+++ go/gocli/wc/main.go
@@ -8,10 +8,16 @@ import (
 	"os"
 )
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
-	if !countLines {
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
+	}
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
+	if countLines {
+		scanner.Split(bufio.ScanLines)
 	}
 	wc := 0
 	for scanner.Scan() {
@@ -22,6 +28,7 @@ func count(r io.Reader, countLines bool) int {
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	wordBytes := flag.Bool("b", false, "Count numbers of bytes")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *wordBytes))
 }
--- go/gocli/wc/main_test.go
+++ go/gocli/wc/main_test.go
@@ -6,21 +6,23 @@ import (
 )
 
 func TestCountWords(t *testing.T) {
-	testCases := [2]struct {
-		name      string
-		word      string
-		count     int
-		countLine bool
+	testCases := [3]struct {
+		name          string
+		word          string
+		expectedCount int
+		countLine     bool
+		countBytes    bool
 	}{
-		{"CountWord", "word1 word2 word3 work4\n", 4, false},
-		{"CountLine", "word1\nword2\nword3", 3, true},
+		{"CountWord", "word1 word2 word3 work4\n", 4, false, false},
+		{"CountLine", "word1\nword2\nword3", 3, true, false},
+		{"CountBytes", "word1\nword2\nword3", 17, false, true},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			res := bytes.NewBufferString(tc.word)
-			wc := count(res, tc.countLine)
-			if wc != tc.count {
-				t.Errorf("Expected %d, got %d instead.\n", tc.count, wc)
+			wc := count(res, tc.countLine, tc.countBytes)
+			if wc != tc.expectedCount {
+				t.Errorf("Expected %d, got %d instead.\n", tc.expectedCount, wc)
 			}
 		})
 	}
